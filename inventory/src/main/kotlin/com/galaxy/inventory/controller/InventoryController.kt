f53e3047c305c4bfe5ed0d6cf30a08d222a102c9
Sabari Krishna V K <sabari@Sabaris-MacBook-Pro.local>
2021-05-07 19:36:27 -0400

    add dsg fetcher

--- inventory/src/main/kotlin/com/galaxy/inventory/controller/InventoryController.kt
+++ inventory/src/main/kotlin/com/galaxy/inventory/controller/InventoryController.kt
@@ -1,9 +1,9 @@
 package com.galaxy.inventory.controller
 
+import com.galaxy.inventory.codegen.types.Inventory
+import com.galaxy.inventory.codegen.types.Sku
 import com.galaxy.inventory.services.InventoryService
-import com.netflix.graphql.dgs.DgsComponent
-import com.netflix.graphql.dgs.DgsQuery
-import com.netflix.graphql.dgs.InputArgument
+import com.netflix.graphql.dgs.*
 import org.springframework.security.access.prepost.PreAuthorize
 
 @DgsComponent
@@ -12,11 +12,26 @@ class InventoryController(private val inventoryService: InventoryService) {
      * This datafetcher resolves the shows field on Query.
      * It uses an @InputArgument to get the titleFilter from the Query if one is defined.
      */
+    @DgsEntityFetcher(name ="Sku")
+    fun skus( values : Map<String,String> ): Sku?{
+        return values.get("skuid")?.let { Sku(it) }
+    }
     @DgsQuery
 //    @Secured("REGISTERED")
 //    @PreAuthorize("hasAnyRole('ROLE_GUEST',)") //Will throw exception
     @PreAuthorize("hasAnyRole('ROLE_REGISTERED','ROLE_GUEST')")
-    fun inventory(@InputArgument skuid : String?,@InputArgument location : String?): List<com.galaxy.inventory.codegen.types.Inventory> {
+    fun inventorySkuLocation(@InputArgument skuid : String?,@InputArgument location : String?): List<Inventory> {
+        return if(skuid != null) {
+            inventoryService.inventory().filter { it.skuid.contains(skuid) }
+        } else {
+            inventoryService.inventory()
+        }
+    }
+    @DgsData(parentType = "Sku" , field="inventory")
+//    @Secured("REGISTERED")
+//    @PreAuthorize("hasAnyRole('ROLE_GUEST',)") //Will throw exception
+    @PreAuthorize("hasAnyRole('ROLE_REGISTERED','ROLE_GUEST')")
+    fun inventory(@InputArgument skuid : String? ): List<Inventory> {
         return if(skuid != null) {
             inventoryService.inventory().filter { it.skuid.contains(skuid) }
         } else {
